3f77c0b8e8f4ee49271de75286f74c0d7da67804
pobbatti vijaya lakshmi <honeyvijji643@gmail.com>
2022-08-18 10:10:24 +0530

    Add project files.

---
+++ FootballCodebased/Models/Football.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+namespace FootballCodebased.Models
+{
+    public class Football
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int MatchID { get; set; }
+        public string TeamName1 { get; set; }
+        public string TeamName2 { get; set; }
+        public string Status { get; set; }
+        public string WinningTeam { get; set; }
+        public int Points { get; set; }
+    }
+}
---
+++ FootballCodebased/Models/FootballContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace FootballCodebased.Models
+{
+    public class FootballContext : DbContext
+    {
+        public FootballContext() : base("DBCS")
+        {
+
+        }
+        public DbSet<Football> Football { get; set; }
+    }
+}
