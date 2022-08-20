cbdac8df1c0eb3338dba06d3144499015293cc82
VPOBBATT <109226642+VPOBBATT@users.noreply.github.com>
2022-08-20 12:56:40 +0530

    Update Football.cs

--- FootballCodebased/Models/Football.cs
+++ FootballCodebased/Models/Football.cs
@@ -10,13 +10,19 @@ namespace FootballCodebased.Models
 {
     public class Football
     {
+        
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int MatchID { get; set; }
+        [Required]
         public string TeamName1 { get; set; }
+        [Required]
         public string TeamName2 { get; set; }
+        [Required]
         public string Status { get; set; }
+        [Compare("FootBall.Status")]
         public string WinningTeam { get; set; }
+        [Required]
         public int Points { get; set; }
     }
 }
